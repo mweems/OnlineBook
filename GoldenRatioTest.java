250b1ec4794d509d9adba08660578630fc98dce7
mweems <Thoughtworker@MW9591.local>
2013-09-11 17:43:02 -0400

    golden ratio and quadratic equation

---
+++ GoldenRatioTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class GoldenRatioTest {
+
+    GoldenRatio formula = new GoldenRatio();
+
+    @Test
+    public void returnTheGoldenRation(){
+        assertEquals("1.62", formula.ratio());
+    }
+}

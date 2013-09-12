eca5f1f980c10ef6180defba8c4b4254b01e0214
mweems <Thoughtworker@MW9591.local>
2013-09-12 10:22:02 -0400

    added exercise to check if an int is a perfect square

---
+++ chapters/Chapter5/PerfectSquare.java
@@ -0,0 +1,11 @@
+public class PerfectSquare {
+
+    public boolean isPerfectSquare(int num){
+        for(int i = 1; i <= num/2; i++){
+            if(i * i == num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ chapters/Chapter5/PerfectSquareTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+
+public class PerfectSquareTest {
+
+    PerfectSquare perfectSquare = new PerfectSquare();
+
+    @Test
+    public void returnTrueWhenPassedInANumberThatIsAPerfectSquare(){
+        assertEquals(true, perfectSquare.isPerfectSquare(9));
+    }
+
+    @Test
+    public void returnFalseWhenPassedInANumberThatIsNotAPerfectSquare(){
+        assertEquals(false, perfectSquare.isPerfectSquare(8));
+    }
+}
